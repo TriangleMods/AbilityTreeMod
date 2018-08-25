ba0c2c440174b0d4270ab821f42689bf988645ff
daihaminkey <daihaminkey@icloud.com>
2018-08-25 16:37:20 +0300

    Package rename

---
+++ src/main/java/com/triangle/abilitytree/proxy/CommonProxy.java
@@ -0,0 +1,22 @@
+package com.triangle.abilitytree.proxy;
+
+public abstract class CommonProxy
+{
+
+	public void preInit()
+	{
+
+	}
+
+
+	public void init()
+	{
+
+	}
+
+
+	public void postInit()
+	{
+
+	}
+}
