5db58ba4de0b466ea0a62c94856ce5ecdf24ac05
daihaminkey <daihaminkey@icloud.com>
2018-09-09 16:50:47 +0300

    Logic integration with capabilities (don't work yet)

---
+++ src/main/java/com/triangle/abilitytree/tree/capabilities/SkillTreeProvider.java
@@ -0,0 +1,39 @@
+package com.triangle.abilitytree.tree.capabilities;
+
+import net.minecraft.nbt.NBTBase;
+import net.minecraft.util.EnumFacing;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.CapabilityInject;
+import net.minecraftforge.common.capabilities.ICapabilitySerializable;
+
+public class SkillTreeProvider implements ICapabilitySerializable<NBTBase>
+{
+	@CapabilityInject(ISkillTree.class)
+	public static final Capability<ISkillTree> SKILL_TREE_CAPABILITY = null;
+
+	private ISkillTree instance = SKILL_TREE_CAPABILITY.getDefaultInstance();
+
+	@Override
+	public boolean hasCapability(Capability<?> capability, EnumFacing facing)
+	{
+		return capability == SKILL_TREE_CAPABILITY;
+	}
+
+	@Override
+	public <T> T getCapability(Capability<T> capability, EnumFacing facing)
+	{
+		return capability == SKILL_TREE_CAPABILITY ? SKILL_TREE_CAPABILITY.<T> cast(this.instance) : null;
+	}
+
+	@Override
+	public NBTBase serializeNBT()
+	{
+		return SKILL_TREE_CAPABILITY.getStorage().writeNBT(SKILL_TREE_CAPABILITY, this.instance, null);
+	}
+
+	@Override
+	public void deserializeNBT(NBTBase nbt)
+	{
+		SKILL_TREE_CAPABILITY.getStorage().readNBT(SKILL_TREE_CAPABILITY, this.instance, null, nbt);
+	}
+}
